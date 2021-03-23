293ab0b03c08a85dcaeb56d5a5d7ad77edd790f2
Frederick Javalera <fjavalera@goldpointsystems.com>
2021-03-23 15:33:43 -0600

    Init commit

---
+++ HowToCreateCustomTextViewPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowToCreateCustomTextViewPractice
+//
+//  Created by GPS Dev on 3/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HowToCreateCustomTextViewPractice/HowToCreateCustomTextViewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateCustomTextViewPracticeApp.swift
+//  HowToCreateCustomTextViewPractice
+//
+//  Created by GPS Dev on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateCustomTextViewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
